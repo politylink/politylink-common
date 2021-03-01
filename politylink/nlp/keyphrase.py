839f496fa125a4575617f1d58aaaa704200d7862
masa <masatoiskw@gmail.com>
2021-03-01 23:26:09 +0900

    POL-257: reflect review

--- politylink/nlp/keyphrase.py
+++ politylink/nlp/keyphrase.py
@@ -10,36 +10,38 @@ class KeyPhraseExtractor:
 
     def __init__(self):
         # load stop words
+        stopwords = set()
+
         p = path.join(path.dirname(__file__), 'stopwords_minutes.txt')
-        stopwords_minutes = self.load_stopwords(p)
+        stopwords |= self.load_stopwords(p)
 
         p = path.join(path.dirname(__file__), 'stopwords_slothlib.txt')
-        stopwords_sloth = self.load_stopwords(p)
+        stopwords |= self.load_stopwords(p)
 
-        stopwords_members = self.get_member_names()
+        stopwords |= self.get_member_names()
+
+        stopwords |= stop_words.STOP_WORDS
+        stopwords = list(stopwords)
 
         # set stop words
         pke.base.lang_stopwords['ja_ginza'] = 'japanese'
-        stopwords = list(stop_words.STOP_WORDS | stopwords_minutes | stopwords_sloth | stopwords_members)
         nltk.corpus.stopwords.words_org = nltk.corpus.stopwords.words
         nltk.corpus.stopwords.words = lambda lang: stopwords if lang == 'japanese' else nltk.corpus.stopwords.words_org(
             lang)
 
-    def load_stopwords(self, path):
-        with open(path, mode='r') as f:
+    def load_stopwords(self, file_path):
+        with open(file_path, mode='r') as f:
             stopwords = set(f.read().splitlines())
         return stopwords
 
     def get_member_names(self):
         client = GraphQLClient()
-        names = ['first_name', 'first_name_hira', 'last_name', 'last_name_hira']
-        members = client.get_all_members(fields=names)
-        stopwords = []
-        for name in names:
-            stopwords.extend([m[name] for m in members])
-        return set(stopwords)
+        fields = ['first_name', 'first_name_hira', 'last_name', 'last_name_hira']
+        members = client.get_all_members(fields=fields)
+        member_names = [m[f] for f in fields for m in members]
+        return set(member_names)
 
-    def get_key_phrase(self, text, n=3):
+    def extract(self, text, n=3):
         """
         Get key phrases from text
         Up to n key phrases will be returned
@@ -52,7 +54,7 @@ class KeyPhraseExtractor:
         extractor.load_document(input=text, language='ja_ginza', normalization=None)
         extractor.candidate_selection(pos={'NOUN', 'PROPN', 'ADJ', 'NUM'})
         extractor.candidate_weighting(threshold=0.74, method='average', alpha=1.1)
-        key_phrase = extractor.get_n_best(n)
-        key_phrase = [k[0] for k in key_phrase]
+        key_phrases = extractor.get_n_best(n)
+        key_phrases = [k[0] for k in key_phrases]
 
-        return key_phrase
+        return key_phrases
