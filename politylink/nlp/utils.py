97157fc4b92620591e9d073940254768cabebbe6
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-03-22 00:01:37 +0900

    POL-270: filter_by_pos

---
+++ politylink/nlp/utils.py
@@ -0,0 +1,19 @@
+import spacy
+
+nlp = spacy.load('ja_ginza')
+
+WORDCLOUD_POS_TAGS = ['ADJ', 'ADV', 'NOUN', 'PROPN']
+
+
+def filter_by_pos(texts, pos_tags):
+    """
+    filter text that includes target Part-Of-Speech tags
+    """
+
+    ret = []
+    for doc in nlp.pipe(texts):
+        for token in doc:
+            if token.pos_ in pos_tags:
+                ret.append(doc.text)
+                break
+    return ret
